378f1c756242ac49ecfe4fbf1a18bb6ab48483a3
Diego Queres <diegoqueres81@gmail.com>
2021-07-30 20:43:07 -0300

    Mudando a direção

--- core/src/net/diegoqueres/mysnake/GameScreen.java
+++ core/src/net/diegoqueres/mysnake/GameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -17,7 +18,7 @@ import net.diegoqueres.mysnake.enums.Direcao;
 import static net.diegoqueres.mysnake.Constants.IDX_CABECA_COBRA;
 import static net.diegoqueres.mysnake.Constants.TEMPO_MOVER_PADRAO;
 
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, GestureDetector.GestureListener {
 
     private Game game;
 
@@ -34,6 +35,8 @@ public class GameScreen implements Screen {
 
     private float tempoParaMover;
 
+    private Vector2 toque;
+
 
     public GameScreen(Game game) {
         this.game = game;
@@ -48,6 +51,10 @@ public class GameScreen implements Screen {
 
         gerarTextura();
         init();
+
+        toque = new Vector2();
+
+        Gdx.input.setInputProcessor(new GestureDetector(this));
     }
 
     @Override
@@ -61,63 +68,64 @@ public class GameScreen implements Screen {
 
         batch.draw(texFundo, 0, 0, 100, 100);
         for (Vector2 parte : partes)
-            batch.draw(texCorpo, parte.x*5, parte.y*5, 5, 5);
+            batch.draw(texCorpo, parte.x * 5, parte.y * 5, 5, 5);
 
         batch.end();
     }
 
     private void update(float delta) {
         tempoParaMover -= delta;
-        if (tempoParaMover <= 0) {
-            tempoParaMover = TEMPO_MOVER_PADRAO;
-            Gdx.app.log("Log", "move");
 
-            int x1, y1, x2, y2;
+        if (tempoParaMover > 0)
+            return;
 
-            x1 = (int) partes.get(IDX_CABECA_COBRA).x;
-            y1 = (int) partes.get(IDX_CABECA_COBRA).y;
+        tempoParaMover = TEMPO_MOVER_PADRAO;
+        Gdx.app.log("Log", "move");
+
+        int x1, y1, x2, y2;
+
+        x1 = (int) partes.get(IDX_CABECA_COBRA).x;
+        y1 = (int) partes.get(IDX_CABECA_COBRA).y;
+        corpo[x1][y1] = false;
+
+        x2 = x1;
+        y2 = y1;
+
+        switch (direcao) {
+            case CIMA:
+                y1++;
+                break;
+            case DIREITA:
+                x1++;
+                break;
+            case BAIXO:
+                y1--;
+                break;
+            case ESQUERDA:
+                x1--;
+                break;
+        }
+
+        if (isMovimentacaoInvalida(x1, y1)) {
+            // perdeu
+            return;
+        }
+
+        partes.get(IDX_CABECA_COBRA).set(x1, y1);
+        corpo[x1][y1] = true;
+
+        for (int i = 1; i < partes.size; i++) {
+            Vector2 parte = partes.get(i);
+            x1 = (int) parte.x;
+            y1 = (int) parte.y;
             corpo[x1][y1] = false;
+
+            parte.set(x2, y2);
+            corpo[x2][y2] = true;
 
             x2 = x1;
             y2 = y1;
-
-            switch (direcao) {
-                case CIMA:
-                    y1++;
-                    break;
-                case DIREITA:
-                    x1++;
-                    break;
-                case BAIXO:
-                    y1--;
-                    break;
-                case ESQUERDA:
-                    x1--;
-                    break;
-            }
-
-            if (isMovimentacaoInvalida(x1, y1)) {
-                // perdeu
-                return;
-            }
-
-            partes.get(IDX_CABECA_COBRA).set(x1, y1);
-            corpo[x1][y1] = true;
-
-            for (int i = IDX_CABECA_COBRA + 1; i < partes.size; i++) {
-                x1 = (int) partes.get(i).x;
-                y1 = (int) partes.get(i).y;
-                corpo[x1][y1] = false;
-
-                partes.get(i).set(x2, y2);
-                corpo[x2][y2] = true;
-
-                x2 = x1;
-                y2 = y1;
-            }
-
         }
-
     }
 
     private boolean isMovimentacaoInvalida(int x, int y) {
@@ -127,6 +135,31 @@ public class GameScreen implements Screen {
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height, true);
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        toque.set(velocityX, velocityY);
+        viewport.unproject(toque);
+        Gdx.app.log("Log", velocityX + " " + velocityY + " " + toque.x + " " + toque.y);
+
+        if (Math.abs(toque.x) > Math.abs(toque.y))
+            toque.y = 0;
+        else
+            toque.x = 0;
+
+        final int minVelocity = 50;
+        if (toque.x > minVelocity && direcao != Direcao.ESQUERDA) {
+            direcao = Direcao.DIREITA;
+        } else if (toque.y > minVelocity && direcao != Direcao.BAIXO) {
+            direcao = Direcao.CIMA;
+        } else if (toque.x < -minVelocity && direcao != Direcao.DIREITA) {
+            direcao = Direcao.ESQUERDA;
+        } else if (toque.y < -minVelocity && direcao != Direcao.CIMA) {
+            direcao = Direcao.BAIXO;
+        }
+
+        return true;
     }
 
     private void limparTela() {
@@ -185,6 +218,46 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
+
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
 
     }
 }
